4a0b5dea1f07b1beb36bf9337997cef0f5faa101
Matthew-Jordon Whynter <matthewjordon.whynter@mymona.uwi.edu>
2022-04-24 20:33:47 -0500

    car details view

---
+++ src/views/CarDetailsView.vue
@@ -0,0 +1,15 @@
+<script>
+    import CarDetails from "../components/CarDetails.vue"
+    export default {
+        data() {
+            return {}
+        },
+        components: { CarDetails }
+    }
+</script>
+
+<template>
+    <div class="container">
+        <CarDetails />
+    </div>
+</template>
